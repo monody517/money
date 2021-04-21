482ba4a901fd4e725c16ffe65cacddadfe5bcc55
Monody517 <2737716714@qq.com>
2021-04-21 17:13:28 +0800

    money模块化

---
+++ src/components/money/Notes.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <label class="notes">
+            <span>备注</span>
+            <input type="text" placeholder="在这里输入备注">
+        </label>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'Notes'
+    }
+</script>
+
+<style lang="scss" scoped>
+.notes{
+    font-size: 14px;
+    background: #f5f5f5;
+    padding-left: 16px;
+    display: flex;
+    align-items: center;
+    > span{
+        padding-right: 16px;
+    }
+    > input{
+        height: 64px;
+        flex-grow: 1;
+        border: none;
+        background: transparent;
+    }
+}
+</style>
---
+++ src/components/money/Types.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <ul class="types">
+            <li class="selected">支出</li>
+            <li>收入</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'Types',
+    }
+</script>
+
+<style lang="scss" scoped>
+.types{
+    background: #c4c4c4;
+    display: flex;
+    text-align: center;
+
+    font-size: 24px;
+    > li{
+        width: 50%;
+        height: 64px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        position: relative;
+        &.selected::after{
+            content: '';
+            position: absolute;
+            width: 100%;
+            height: 4px;
+            background: #444;
+            bottom: 0;
+            left: 0;
+        }
+    }
+}
+</style>
